55080dfe45270e55c7be3e76681fabbce1e3895c
jiangtianan <jiangtianan@noreply.gitcode.com>
2026-03-07 10:27:09 +0800

    vault backup: 2026-03-07 10:27:09

--- 后端/20.python/02.python基础/代码/06_名片管理/cards_main.py
+++ 后端/20.python/02.python基础/代码/06_名片管理/cards_main.py
@@ -1,17 +1,20 @@
+import cards_tools
+
 while True:
   # TODO 显示功能菜单
+  cards_tools.show_menu()
 
-  action_str = input("请选择希望执行的操作：\n1. 创建名片\n2. 显示全部名片\n3. 查询名片\n4. 修改名片\n5. 删除名片\n请输入数字选择操作：")
+  action_str = input("请选择希望执行的操作：")
 
   #1,2,3 针对名片的操作
   if action_str in ["1", "2", "3"]:
       from cards_tools import *
       if action_str == "1":
-          create_card()
+          cards_tools.create_card()
       elif action_str == "2":
-          show_all()
+          cards_tools.show_all()
       elif action_str == "3":
-          search_card()
+          cards_tools.search_card()
 
   # 0 退出系统
   elif action_str == "0":
--- 后端/20.python/02.python基础/代码/06_名片管理/cards_tools.py
+++ 后端/20.python/02.python基础/代码/06_名片管理/cards_tools.py
@@ -8,8 +8,24 @@ def show_menu():
     print("1. 创建名片")
     print("2. 显示全部名片")
     print("3. 查询名片")
-    print("4. 修改名片")
-    print("5. 删除名片")
     print("")
     print("0. 退出系统")
     print("*" * 50)
+
+"""
+创建名片
+"""
+def create_card():
+    print("创建名片")
+
+"""
+显示全部名片
+"""
+def show_all():
+    print("显示全部名片")
+
+"""
+查询名片
+"""
+def search_card():
+    print("查询名片")
