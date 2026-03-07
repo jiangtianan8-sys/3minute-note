63de75ec907ed7136de5b1066e986427b6c81d39
jiangtianan <jiangtianan@noreply.gitcode.com>
2026-03-07 10:14:02 +0800

    vault backup: 2026-03-07 10:14:02

---
+++ 后端/20.python/02.python基础/代码/06_名片管理/cards_tools.py
@@ -0,0 +1,15 @@
+"""
+显示菜单
+"""
+def show_menu():
+    print("*" * 50)
+    print("欢迎使用【名片管理系统】V1.0")
+    print("")
+    print("1. 创建名片")
+    print("2. 显示全部名片")
+    print("3. 查询名片")
+    print("4. 修改名片")
+    print("5. 删除名片")
+    print("")
+    print("0. 退出系统")
+    print("*" * 50)
